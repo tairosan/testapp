5f957db16b79e6cb593435e37c380c38946d59e3
tairosan <tairosan@gmail.com>
2013-11-11 21:42:29 +0900

    jquery masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_filter :authenticate_user!, except: [:index]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
