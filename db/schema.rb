b86ba07f8c68f43b64b6852aa573992106985e6a
tairosan <tairosan@gmail.com>
2013-12-06 16:44:47 +0900

    before sendgrid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114090532) do
+ActiveRecord::Schema.define(:version => 20131127043928) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(:version => 20131114090532) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "provider"
+    t.integer  "fbid"
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
